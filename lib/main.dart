db9ca54a2fa438079d0529d34c496d9bed78a98b
Muhsin Yaşar <2106102010@std.idu.edu.tr>
2022-05-21 00:47:51 +0300

    Düzeltme2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meetime_app/swap_screen_into.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
